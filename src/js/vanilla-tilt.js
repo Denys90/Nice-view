b34ab9836eaff836225be8cf4131b0dc3650961c
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2023-11-02 00:35:16 +0100

    Added vamilla-tilt

---
+++ src/js/vanilla-tilt.js
@@ -0,0 +1,7 @@
+import VanillaTilt from 'vanilla-tilt';
+VanillaTilt.init(document.querySelector('.box'), {
+  max: 25,
+  speed: 400,
+});
+console.log('Vanilla Tilt ініціалізовано');
+console.log('asdasdasd');
